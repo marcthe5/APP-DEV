613869ad69bd4f3a917a12214a178646f85a7d99
Angebiscante <118629828+Angebiscante@users.noreply.github.com>
2023-03-20 11:54:23 +0800

    Add files via upload
    2 PHASE

---
+++ views/bizpartner/css/bizpartner_css.blade.php
@@ -0,0 +1,14 @@
+<style>
+   
+    #CREATE_BP_PAGE-cont{
+		overflow-y:hidden !important;
+	}
+	.sapMInputBaseReadOnly. .sapMInputBaseContentWrapper {
+		border-color : #ccc;
+		background: none;
+		border: 1px transparent;
+	}
+
+  
+
+</style>
