3a3c7f68bdb48a3a13499bef8910ec74b73b396a
Casoco, Marc T <77011668+marcthe5@users.noreply.github.com>
2023-04-03 10:52:45 +0800

    Add files via upload
    NEW NEW NEW

--- resources/views/bizpartner/css/AppDelivery_css.blade.php
+++ resources/views/bizpartner/css/AppDelivery_css.blade.php
@@ -2,18 +2,16 @@
    
     #CREATE_BP_PAGE-cont{
 		overflow-y:hidden !important;
+		
 	}
 	.sapMInputBaseReadOnly{
-		border-color : none;
-		background: none;
 		border: 1px transparent;
-		margin: 5px;
-		float: left;
+		background-color: none;
+		translate: translateY(50%);
 
-	}.sapMInputBaseContentWrapper{
+	}
+	.sapMInputBaseContentWrapper{
 		border: 5px;
-		margin: 5px;
-		float: left;
 
 	 }
 	
@@ -22,13 +20,41 @@
 		font-family: Arial;
 		font-weight: bolder;
 		color: white;
-       background-color: gray;
+       background-color: #7e98ba;
 	}
 	.class_input{
 		border: 6vw;
 		border-color: red;
 	}
 
+
+
+.sapUiLocalBusyIndicator:focus:after {
+content: "";
+position: absolute;
+left: 1px;
+top: 1px;
+right: 1px;
+bottom: 1px;
+border: 2px solid transparent;
+pointer-events: none
+}
+.sapMTB-Transparent-CTX{
+	transform: translateX(30%);
+	background-color: silver;
+	background: cover;
+}
+.nav_list{
+	font-size: 50px;
+}
+.sapUiSizeCompact {
+	background-color: #7e98ba;
+	border:none;
+	color: white;
+}
+.menu_title{
+	color: red;
+}
   
 
 </style>
