475448b467053a73e7b4f88b0aa33fb74c2ddcdd
Casoco, Marc T <77011668+marcthe5@users.noreply.github.com>
2023-05-18 12:00:03 +0800

    Add files via upload

---
+++ global_template.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <title>@yield('title')</title>
+    <script src="{!! asset('library/resources/sap-ui-core.js') !!}"
+            id="sap-ui-bootstrap"                
+			data-sap-ui-libs="sap.m,sap.ui.commons,sap.ui.unified,sap.ui.ux3,sap.ui.table, sap.uxap, sap.tnt"
+            data-sap-ui-theme="sap_bluecrystal">
+    </script>
+        <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}" />
+    @yield('styles')
+    @yield('content')
+</head>
+<body class="sapUiBody" role="application">	
+	<div id="invisible_content" style="display:none;"></div>
+	<div id="google_translate_element"></div>
+    <div id="content"></div>      
+	@yield('footer')	   
+</body>
+</html>
