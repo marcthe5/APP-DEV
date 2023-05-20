52a98b1978e7ca91fe1cc09eac9f9da40dd9f8ef
Casoco, Marc T <77011668+marcthe5@users.noreply.github.com>
2023-05-20 14:16:37 +0800

    Add files via upload

---
+++ delivery.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2023 at 08:16 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appdev_delivery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `DELIVERY_ID` varchar(20) NOT NULL,
+  `DELIVERY_ADDRESS` varchar(150) DEFAULT NULL,
+  `EMP_NAME` varchar(50) DEFAULT NULL,
+  `EMP_PHONE` varchar(15) DEFAULT NULL,
+  `EMP_EMAIL` varchar(50) DEFAULT NULL,
+  `EMP_DELIVERY_DATE` varchar(150) DEFAULT NULL,
+  `EMP_DELIVERY_TIME` varchar(150) DEFAULT NULL,
+  `EMP_DELIVERY_INSTRUCTION` varchar(150) DEFAULT NULL,
+  `EMP_SHIPPING_METHOD` varchar(50) DEFAULT NULL,
+  `EMP_SHIPPING_CARRIER` varchar(50) DEFAULT NULL,
+  `EMP_TRACKING_NUMBER` varchar(50) DEFAULT NULL,
+  `EMP_PACKAGE_WEIGHT` varchar(50) DEFAULT NULL,
+  `EMP_PACKAGE_DIMENSION` varchar(50) DEFAULT NULL,
+  `EMP_DELIVERY_CONFIRMATION` varchar(50) DEFAULT NULL,
+  `EMP_SIGNATURE_REQUIRED` varchar(50) DEFAULT NULL,
+  `EMP_ORDER_NUMBER` varchar(50) DEFAULT NULL,
+  `EMP_SHIPPING_COST` varchar(50) DEFAULT NULL,
+  `EMP_INSURANCE` varchar(50) DEFAULT NULL,
+  `EMP_CUSTOMS_INFO` varchar(50) DEFAULT NULL,
+  `EMP_ORDER_STATUS` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`DELIVERY_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
